f56b14524ba607aa52261a27616f815dbdec986b
Guyiorio <50681511+Guyiorio@users.noreply.github.com>
2020-05-03 21:43:03 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+
+  <head>
+
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+  </head>
+
+
+  <body>
+
+    <div id="content">
+
+     <?php include('content.php'); ?>
+
+    </div>
+
+
+  </body>
+
+</html>
